ada524c12d063ae3b9987d914257fff57b477ff3
kim eun gyeong <dmsrud518@naver.com>
2024-01-07 15:23:11 +0900

    use effect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once");
+  };
+  useEffect(() => {
+    console.log("call the api");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>welcome back!</h1>
-      <Button text={"continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>test</button>
     </div>
   );
 }
