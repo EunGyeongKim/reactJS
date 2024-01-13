6d6db157b63d0dfdcdd17b9201c3f97d2ed313c5
kim eun gyeong <dmsrud518@naver.com>
2024-01-13 21:00:05 +0900

    Cleanup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useInsertionEffect } from "react";
 
+function Hello() {
+  function byeFn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byeFn;
+  }
+  useEffect(hiFn, []);
+  return <h1> Hello</h1>;
+}
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  useEffect(() => {
-    console.log("i run only once");
-  }, []);
-
-  useEffect(() => {
-    console.log("i run when 'keyword' changes. ");
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("i run when 'counter' changes. ");
-  }, [counter]);
-  useEffect(() => {
-    console.log("i run when 'counter'& 'keyword' changes. ");
-  }, [counter, keyword]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>test</button>
+      <button onClick={onClick}> {showing ? "hide" : "show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
