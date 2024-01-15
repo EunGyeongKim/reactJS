c027500d92af989e8303709a1ff829c4f9745034
kim eun gyeong <dmsrud518@naver.com>
2024-01-15 22:30:38 +0900

    movie app part one

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,42 @@
-import { useState, useEffect, useInsertionEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [dallors, setDallors] = useState(0);
-  const dallorsOnChange = (event) => {
-    setDallors(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
-
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   return (
     <div>
-      <h1> The coins! {loading ? "" : `(${coins.length})`}</h1>
-
-      <input
-        value={dallors}
-        onChange={dallorsOnChange}
-        type="text"
-        placeholder="input your dallors"
-      ></input>
-
       {loading ? (
-        <strong> loading..... </strong>
+        <h1>Loading...</h1>
       ) : (
-        coins.map((coin, index) => (
-          <li
-            style={{ listStyleType: "none" }}
-            key={index}
-            value={coin.quotes.USD.price}
-          >
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
-            {dallors == "" ? "" : ` / ${dallors / coin.quotes.USD.price}`}
-          </li>
-        ))
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
 }
+
 export default App;
